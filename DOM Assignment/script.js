7bf8c913ffd613f1242ae9f423f41cae08c82334
vamshivaasu <vamshivaasu1999@gmail.com>
2023-01-23 15:05:41 +0530

    maps and sets

---
+++ DOM Assignment/script.js
@@ -0,0 +1,18 @@
+let chkbtn=document.getElementById("btn")
+let ranNum=document.getElementById("between")
+let input=document.getElementById("guessid")
+let hint=document.getElementById("sc")
+
+let inpvalue=input.value;
+ranNum.value=parseInt(Math.random*100+1)
+let randomvalue=ranNum.value;
+
+chkbtn.addEventListener("click",function(){
+
+    if(inpvalue>randomvalue){
+        sc.innerText="Your Guess is High"
+    }
+    else{
+        sc.innerText="Your Guess is Low"
+    }
+})
